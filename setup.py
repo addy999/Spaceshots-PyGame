b6cb874cd6a4ffb3ff50e981d1244b04a7196d35
Addy Bhatia <addy@loftorbital.com>
2019-07-18 19:13:50 -0400

    pip package prep

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ import os,sys
 from setuptools import setup
 
 # For regular package imports, not 'import .packagename'
-modpath = os.path.abspath(os.path.split(sys.argv[0])[0]) + '/astron'
-sys.path.append(modpath)
-import astron
+# modpath = os.path.abspath(os.path.split(sys.argv[0])[0]) + '/astron'
+# sys.path.append(modpath)
+# import astron
 
-setup(name=astron.__name__,
-      version=astron.__version__,
+setup(name='astron',
+      version='0.1.9.3',
       description='...',
-      url='...',
-      author='...',
-      author_email='',
+      url='https://www.python.org',
+      author='Addy Bhatia',
+      author_email='jude.addy999@gmail.com',
       license='MIT',
-      install_requires=['autopep8', 'pyganme', 'numpy'],
-      packages=['gravityassist'], 
+      install_requires=['autopep8', 'pygame', 'numpy'],
+      packages=['astron'], 
       include_package_data=True,
       zip_safe=True)
