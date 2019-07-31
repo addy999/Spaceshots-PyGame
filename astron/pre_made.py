7b87b088faa0b2aa4d268751140b452242dd6203
Addy Bhatia <addy@loftorbital.com>
2019-07-31 09:16:19 -0400

    new level added

--- astron/pre_made.py
+++ astron/pre_made.py
@@ -20,7 +20,7 @@ screen_x, screen_y = 1366, 768
 
 ############# LEVEL 1 #############
 
-sc = Spacecraft('Test', mass = 100, thrust_force = 3000, gas_level = 2000)
+sc = Spacecraft('Test', mass = 100, thrust_force = 3000, gas_level = 600)
 orbit = Orbit(a=screen_x*500/1920, b=screen_y*500/1080, center_x=screen_x, center_y=screen_y/2, CW=True, angular_step = 2*np.pi/(200.0), progress = np.pi/2)
 planet = Planet('Test', mass = 3e16, orbit = orbit)
 level1 = GameScene(resolution = (screen_x, screen_y), sc=sc, planets=[planet], win_region = ([0,0], [screen_x, 0]), win_velocity = 90.0,
@@ -77,12 +77,28 @@ level5 = GameScene(resolution = (screen_x, screen_y), sc=sc, planets=[planet, pl
 )
 ##################################
 
+sc = Spacecraft('Test', mass = 100, thrust_force = 5500, gas_level = 250)
+
+orbit = Orbit(a=screen_y*800/1080, b=screen_y*800/1080, center_x=0.0, center_y=3*screen_y/4, CW=False, angular_step = np.pi/(200.0), progress = 0.2)
+planet = Planet('Test', mass = 5e16/1.4, orbit = orbit, color = (8, 204, 15))
+
+orbit2 = Orbit(a=screen_y*800/1080, b=screen_y*800/2080, center_x=screen_x/2, center_y=0, CW=False, angular_step = 3*np.pi/(100.0), progress = np.pi*1.5)
+planet2 = Planet('Test', mass = 8e16/1.4, orbit = orbit2, color = (255, 162, 0))
+
+level6 = GameScene(resolution = (screen_x, screen_y), sc=sc, planets=[planet, planet2], 
+                   win_region = ([screen_x*0.75,0], [screen_x, 0.0]),
+                   win_velocity = 100, 
+           background = getModpath() + r'/images/stars_6.jpg', sc_start_pos = [0.0, screen_y]
+)
+##################################
+
 game = Game(scenes = [
         level1, 
         level2, 
+        level6,
         level3, 
         level4,
-        level5
+        level5,        
         ], fullscreen=True, fps=60)
 
 # game.startGame(splash=True)
